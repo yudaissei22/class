857df0eb6ff15944e1f3efef131808a57d7cf417
yudaissei22 <yuda@jsk.imi.u-tokyo.ac.jp>
2023-05-21 00:53:49 +0900

    a

--- soft/ass1.py
+++ soft/ass1.py
@@ -5,31 +5,34 @@
 #check a todo items
 #新規登録でき、一覧でき、削除もできる。
 
-from flask import Flask, render_template, request, redirect
+
+from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__)
+todos = []
 
-# Create a list to store the tasks
-data = []
-
-# Define the route for the home page
 @app.route('/')
 def index():
-    return render_template('index.html', tasks=tasks)
+        return render_template('index.html', todos=todos)
 
-# Define the route to handle task submission
-@app.route('/add_task', methods=['POST'])
-def add_task():
-    task = request.form['task']
-    tasks.append(task)
-    return redirect('/')
+@app.route('/todos')
+def todo_list():
+        return render_template('todos.html', todos=todos)
+    
 
-# Define the route to handle task deletion
-@app.route('/delete_task', methods=['POST'])
-def delete_task():
-    task = request.form['task']
-    tasks.remove(task)
-    return redirect('/')
+@app.route('/add', methods=['POST'])
+def add():
+    todo = request.form.get('todo')
+    todos.append(todo)
+    return redirect(url_for('index'))
+
+@app.route('/delete/<int:index>', methods=['GET'])
+def delete(index):
+        if index < len(todos):
+        del todos[index]
+        return redirect(url_for('index'))
+
+
 
 if __name__ == '__main__':
 
