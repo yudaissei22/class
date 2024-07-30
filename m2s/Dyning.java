26a228d33d05d8dad82cad3ba7799d19cc72e1a0
yuda <yuda@jsk.imi.i.u-tokyo.ac.jp>
2024-07-30 19:49:41 +0900

    all

---
+++ m2s/Dyning.java
@@ -0,0 +1,99 @@
+class Phil implements Runnable{
+    int x,y;
+    int id;
+    Fork left, right;
+
+    public Phil(int id, int x, int y, Fork l, Fork r){
+	this.id = id;
+        this.x = x;
+        this.y = y;
+        this.left = left;
+        this.right = right;
+    }
+
+    public void draw(){
+	strokeWeight(10);
+	if (left.owner == this) line(x, y, left.x, left.y);
+	if (right.owner == this) line(x, y, right.x, right.y);
+    }
+    public void rsleep(int m){
+	try{
+	    Thread.sleep((int)random(m+100));
+	}
+	catch (Exception ex){
+	}
+    }
+
+    public void sleep(int m){
+	try {
+	    Thread.sleep(m)
+		}
+	catch (Exception ex){
+	}
+    }
+
+    public void run(){
+	while (true){
+	    rsleep(1500);
+	    left.pick(this);
+	    println("" + this.id + "picked left fork" + left.id);
+	    rsleep(1000);
+	    right.pick(this);
+	    println("" + this.id + "picked right fork" + right.id);
+	    rsleep(1000);
+	    println("" + this.id + "drops right fork" + right.id);
+	    right.drop(this);
+	    println("" + this.id + "drops left fork" + left.id);
+	    left.drop(this);
+	}
+    }
+}
+
+class Fork{
+    int id;
+    int x,y;
+    Phil owner = null;
+
+    public Fork(int id, int x, int y){
+	this.id = id;
+	this.x = x;
+	this.y = y;
+    }
+
+    public synchronized void pick(Phil me) {
+	while (owner != null){
+	    // println("" + me.id + "waiting for Fork" + id + "from Phil:" + owner.id);
+	    try{
+		this.wait();
+	    }
+	    catch (Exception ex){
+		println(ex);
+	    }
+	}
+	owner = me;
+
+	public synchronized void drop(Phil me) {
+		if (owner == me) {
+		    owner = null;
+		    this.notify();
+		}
+	    }
+    }
+
+    Fork[] fork;
+    Phil[] phil;
+
+    PImage table;
+    
+    void setup() {
+	table = loadImage("table.jpg");
+	size(482,500);
+
+	phil = new Phil[5];
+	fork = new Fork[5];
+
+	
+	    
+	
+		
+	
