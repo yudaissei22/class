778ad23d5f44d6cb431e63283b200b8700bda30d
yuda <yuda@jsk.imi.i.u-tokyo.ac.jp>
2024-07-30 20:32:02 +0900

    all

--- m2s/2/rpn.py
+++ m2s/2/rpn.py
@@ -1,6 +1,34 @@
-# 演算子が来たら、スタックから2個とって、計算結果を、積む。
-
 from collections import deque
 from queue import Queue
 import time
 
+# 演算子が来たら、スタックから2個とって、計算結果を、積む。
+
+def RPN(states):
+    operator = {
+        '+': (lambda x, y: x + y),
+        '-': (lambda x, y: x - y),
+        '*': (lambda x, y: x * y),
+        '/': (lambda x, y: float(x) / y)
+    }
+    stack = []
+    print('RPN: %s' % states)
+    for index, z  in enumerate(states):
+        if index > 0:
+            print(stack)
+        if z not in operator.keys():
+            stack.append(int(z))
+            continue
+        y = stack.pop()
+        x = stack.pop()
+        stack.append(operator[z](x, y))
+        print('%s %s %s =' % (x, z, y))
+    print(stack[0])
+    return stack[0]
+def test():
+    print("OK" if RPN("37+621-*+") == 16 else "NG")
+
+if __name__ == '__main__':
+    import sys
+    RPN(sys.argv[1])
+    test()
