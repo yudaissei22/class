ab675d9ee41e039918536e8f0671547b8a1999af
yuda <yuda@jsk.imi.i.u-tokyo.ac.jp>
2024-07-30 20:29:28 +0900

    all

---
+++ m2s/2/hanoi.py
@@ -0,0 +1,7 @@
+def hanoi(n, a, b, c):
+    if n > 1:
+        hanoi(n-1, a, b, c)
+        print("move %s: %s --> %s" %(n,a,c))
+        hanoi(n-1, b, a, c)
+
+hanoi(4,"A","B","C")
---
+++ m2s/2/rpn.py
@@ -0,0 +1,6 @@
+# 演算子が来たら、スタックから2個とって、計算結果を、積む。
+
+from collections import deque
+from queue import Queue
+import time
+
